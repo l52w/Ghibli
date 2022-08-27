a36ed282ef2b8c5bd22e92674ac75d7140173807
예인 <kuru040611@gmail.com>
2022-08-28 01:32:02 +0900

    00

--- js/script.js
+++
@@ -1,6 +0,0 @@
-var currentIndex = 0;
-setInterval(function(){
-    if(currentIndex < 2){currentIndex++;} else {currentIndex = 0;}
-    var slidePosition = currentIndex * (-750)+"px";
-    $(".slideList").animate({ top: slidePosition },500);
-},3000);
